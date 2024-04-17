3991d72b9cea5aa7b1c949aaecb4fe489da2432b
Vitaliy Zaytsev <vitaliy@stashaway.com>
2024-04-17 17:36:58 +0300

    copied keymap from my qmk_firmware fork

---
+++ keyboards/crkbd/keymaps/kroleg/config.h
@@ -0,0 +1,23 @@
+#pragma once
+
+//#define USE_MATRIX_I2C
+#ifdef KEYBOARD_crkbd_rev1_legacy
+#    undef USE_I2C
+#    define USE_SERIAL
+#endif
+
+/* Select hand configuration */
+
+// #define MASTER_LEFT
+#define MASTER_RIGHT
+// #define EE_HANDS
+
+// TODO what is this?
+#define USE_SERIAL_PD2
+
+/* TODO check if *_HOLD still needed https://docs.qmk.fm/#/breaking_changes
+ this was last built on 0.14.6 */
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+// Apply the modifier on keys that are tapped during a short hold of a modtap.
+#define PERMISSIVE_HOLD
