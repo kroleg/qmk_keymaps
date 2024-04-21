ab0f1b33072fdfe26bd5fe62a345ab94aa805727
Vitaliy Zaytsev <teh.kroleg@gmail.com>
2024-04-21 10:53:26 +0000

    use 3rd thum on the right as ohe shot hyper

--- keyboards/crkbd/keymaps/kroleg/keymap.c
+++ keyboards/crkbd/keymaps/kroleg/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_LBRC, KC_P,
 		HOME_A,  QH_S,    QH_D,    QH_F,    KC_G,        KC_H,    QH_J,    QH_K,    QH_L,    QH_SCLN,
 		KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_NO,
-                          KC_TAB,  MO(_SYM_RU), KC_ENT,  SFT_SPC, MO(_NAV), KC_NO
+                          KC_TAB,  MO(_SYM_RU), KC_ENT,  SFT_SPC, MO(_NAV), OSM(MOD_HYPR)
 	),
     [_COLEMAK] = LAYOUT_split_3x5_3(
 		KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,        KC_J,    KC_L,    KC_U,    KC_Y,    KC_NO,
