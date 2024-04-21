c3c41208860a14642493881a88165928f82ea5aa
Vitaliy Zaytsev <vitaliy@stashaway.com>
2024-04-21 10:47:58 +0000

    add CONVERT_TO option to avoid adding -e CONVERT_TO to qmk compile calls

--- keyboards/crkbd/keymaps/kroleg/rules.mk
+++ keyboards/crkbd/keymaps/kroleg/rules.mk
@@ -9,3 +9,7 @@ OLED_ENABLE = no
 ENCODER_ENABLE = no
 # https://docs.qmk.fm/#/feature_caps_word
 CAPS_WORD_ENABLE = yes
+# because i am using black promicro rp2040 same as https://mechboards.co.uk/products/pro-micro-5v
+# and they advise to use rp2040_ce
+# see pro-micro-rp2040-pinout.webp
+CONVERT_TO=rp2040_ce
