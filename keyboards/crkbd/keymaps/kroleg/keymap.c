85d4271f0bd27f934ec031e796014031de055b97
Vitaliy Zaytsev <teh.kroleg@gmail.com>
2025-03-25 06:38:41 +0000

    enable chordal & permissive hold

--- keyboards/crkbd/keymaps/kroleg/keymap.c
+++ keyboards/crkbd/keymaps/kroleg/keymap.c
@@ -85,6 +85,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	)
 };
 
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
+    LAYOUT_split_3x5_3(
+        'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R',
+                  '*', '*', '*',  '*', '*', '*'
+    );
+
 /*
  * combos to make it easier to use homerow mods
  */
