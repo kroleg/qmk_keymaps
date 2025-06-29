0bd9d9b55f8a147c3c912bff8131d5b9f121e2e1
Vitaliy Zaytsev <vitaliy@stashaway.com>
2025-06-29 09:09:47 +0000

    correct convert_to

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ This is a fork of qmk/qmk_userspace i use to compile keymaps without having to f
 1. start devcontainer using vscode
 1. run setup `./.devcontainer/setup.sh`. Maybe `qmk setup` will work instead
 1. Add a new keymap for your board using `qmk new-keymap` or copy one from you qmk fork (ex: `keyboards/crkbd/keymaps/<your keymap name>`).
-1. run `qmk compile -kb crkbd -km kroleg -e CONVERT_TO=promicro_rp2040`
+1. run `qmk compile -kb crkbd -km kroleg`
--- keyboards/crkbd/keymaps/kroleg/rules.mk
+++ keyboards/crkbd/keymaps/kroleg/rules.mk
@@ -11,5 +11,6 @@ COMBO_ENABLE = yes
 # https://docs.qmk.fm/#/feature_caps_word
 CAPS_WORD_ENABLE = yes
 # i use promicro rp2040 same as https://mechboards.co.uk/products/pro-micro-5v (see pro-micro-rp2040-pinout.webp)
-# mechboards advise to use rp2040_ce but LED_PIN doesn't work with it
-CONVERT_TO=promicro_rp2040
+# previously it worked with promicro_rp2040 but it's deprecated and replaced by sparkfun_pm2040
+# proof https://github.com/qmk/qmk_firmware/blame/177e461d903e6414e6d7b5b80eb36f48b0d68626/platforms/chibios/converters/promicro_to_promicro_rp2040/pre_converter.mk#L5
+CONVERT_TO=sparkfun_pm2040
