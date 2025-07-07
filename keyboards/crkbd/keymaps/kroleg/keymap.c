b5e4f24952b100749b80eea28d309ef0f8e12075
Vitaliy Zaytsev <vitaliy@stashaway.com>
2025-07-07 21:30:44 +0300

    use CAPS for switching layout (lang in macos)
    ctr+space has some weird behavior when it switches layout twice (i.e. goes back) for one keypress

--- keyboards/crkbd/keymaps/kroleg/keymap.c
+++ keyboards/crkbd/keymaps/kroleg/keymap.c
@@ -278,12 +278,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case SW_LANG:
         if (record->event.pressed) {
             // when keycode SW_LANG is pressed
-            register_code(KC_LCTL);
-            register_code(KC_SPC);
+            tap_code(KC_CAPS);
         } else {
             // when keycode SW_LANG is released
-            unregister_code(KC_SPC);
-            unregister_code(KC_LCTL);
             if (biton32(default_layer_state) == _COLEMAK) {
                 gpio_write_pin(LED_PIN, 1);
                 set_single_persistent_default_layer(_QWERTY);
