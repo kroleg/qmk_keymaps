fbc6c62b7aa97e469e070214d02ccc310495f8ea
Vitaliy Zaytsev <vitaliy@stashaway.com>
2025-07-07 21:47:13 +0300

    adding caps combo to switch macos layout instead of ctrl+space

--- keyboards/crkbd/keymaps/kroleg/keymap.c
+++ keyboards/crkbd/keymaps/kroleg/keymap.c
@@ -105,9 +105,11 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
  */
 const uint16_t PROGMEM cmd_shift_i[] = {HOME_S, HOME_T, HOME_I, COMBO_END};
 const uint16_t PROGMEM cmd_shift_e[] = {HOME_S, HOME_T, HOME_E, COMBO_END};
+const uint16_t PROGMEM combo_u_y[] = {KC_U, KC_Y, COMBO_END};
 combo_t key_combos[] = {
     COMBO(cmd_shift_i, LSFT(LGUI(KC_I))),
     COMBO(cmd_shift_e, LSFT(LGUI(KC_E))),
+    COMBO(combo_u_y, KC_CAPS),
 };
 /* end of combos */
 
